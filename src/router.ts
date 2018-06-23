3aab34f647592cc89748f9c24d400a1479cb9fc5
krrrr38 <k.kaizu38@gmail.com>
2018-06-23 19:10:48 +0900

    ga

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,12 @@
 import Vue from 'vue';
+import VueAnalytics from 'vue-analytics';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 import About from './views/About.vue';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -19,3 +20,10 @@ export default new Router({
     },
   ],
 });
+
+Vue.use(VueAnalytics, {
+  id: 'UA-37666832-1',
+  router,
+});
+
+export default router;
