7edd204ed955af3e06aa871bd43345dacf54f87e
krrrr38 <k.kaizu38@gmail.com>
2018-06-23 18:17:55 +0900

    setup circleci

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -6,6 +6,8 @@ import { home } from './modules/home';
 import { createLogger } from '../plugins/logger';
 
 const debug = process.env.NODE_ENV !== 'production';
+const version = process.env.CIRCLE_SHA1 || '0.0.1';
+console.log({version: version}); // tslint:disable-line
 
 Vue.use(Vuex);
 
@@ -13,9 +15,7 @@ export default new Vuex.Store<RootState>({
   modules: {
     home,
   },
-  state: {
-    version: '0.1.0',
-  },
+  state: { version },
   mutations: {
 
   },
