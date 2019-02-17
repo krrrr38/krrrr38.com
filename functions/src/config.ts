47a2105ceaa7e875bb37e2a36af7890f538c275e
krrrr38 <k.kaizu38@gmail.com>
2019-02-17 13:29:01 +0900

    add function production config

--- functions/src/config.ts
+++ functions/src/config.ts
@@ -6,6 +6,11 @@ import * as functions from 'firebase-functions';
 // firebase functions:config:set slackbot.moon.app_signing_secret=${SLACK_APP_SIGNING_SECRET}
 // firebase functions:config:get > .runtimeconfig.json
 
+// for production
+// firebase -P prod functions:config:set linebot.teto.token=${CHANNEL_ACCESS_TOKEN}
+// firebase -P prod functions:config:set linebot.teto.secret=${CHANNEL_SECRET}
+// firebase -P prod functions:config:set slackbot.moon.app_signing_secret=${SLACK_APP_SIGNING_SECRET}
+
 export const LineBotTetoConfig = {
   channelAccessToken: functions.config().linebot.teto.token,
   channelSecret: functions.config().linebot.teto.secret,
